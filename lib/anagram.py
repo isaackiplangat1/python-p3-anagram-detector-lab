3dca7ae2ebd945e55b34a3b15e42319ceba5a80b
5000vw <isaackiplangat1999@gmail.com>
2023-12-03 19:59:22 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        anagrams = []
+
+        for candidate in words:
+            if self.is_anagram(candidate):
+                anagrams.append(candidate)
+
+        return anagrams
+
+    def is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+
+        return candidate_lower != self.word and sorted(candidate_lower) == sorted(self.word)
